f67e42dfb6c5402d690a499e67aaff77b6753f3b
leo <arokleo@gmail.com>
2022-03-01 21:22:42 +0530

    Init Commit

---
+++ DaysLeft.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace DaysLeft
+{
+    partial class DaysLeft
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.Color.DimGray;
+            this.dateTimePicker1.CalendarTitleForeColor = System.Drawing.Color.DimGray;
+            this.dateTimePicker1.Location = new System.Drawing.Point(57, 159);
+            this.dateTimePicker1.MinDate = new System.DateTime(2021, 12, 8, 15, 21, 6, 194);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(186, 31);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 12, 8, 16, 0, 31, 873);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(83, 218);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.ShowUpDown = true;
+            this.dateTimePicker2.Size = new System.Drawing.Size(140, 31);
+            this.dateTimePicker2.TabIndex = 1;
+            this.dateTimePicker2.Value = new System.DateTime(2021, 12, 8, 16, 0, 31, 875);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(26, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 48);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter End Date";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Algerian", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(67, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 93);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "No \r\nTime\r\nLeft";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(264, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(97, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 34);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // DaysLeft
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 465);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DaysLeft";
+            this.ShowInTaskbar = false;
+            this.Text = "DaysLeft";
+            this.Load += new System.EventHandler(this.DaysLeft_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Label label1;
+        private Button button1;
+        private Label label2;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ DaysLeft.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace DaysLeft
+{
+    public partial class DaysLeft : Form
+    {
+        private bool drag = false; // determine if we should be moving the form
+        private Point startPoint = new Point(0, 0); // also for the moving
+        private DateTime futureDate = DateTime.Now;
+        public DaysLeft()
+        {
+            this.FormBorderStyle = FormBorderStyle.None; // get rid of the standard title bar
+
+            // finally, wouldn't it be nice to get some moveability on this control?
+            this.MouseDown += new MouseEventHandler(Title_MouseDown);
+            this.MouseUp += new MouseEventHandler(Title_MouseUp);
+            this.MouseMove += new MouseEventHandler(Title_MouseMove);
+            this.BackColor = Color.DimGray;
+            this.TransparencyKey = Color.DimGray;
+            this.BackgroundImage = Image.FromFile(Path.Combine(Application.StartupPath, @"..\..\..\Resourse\Background.png"));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.WindowState = FormWindowState.Normal;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            InitializeComponent();
+            this.Reset();
+        }
+
+        void Title_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.drag = false;
+        }
+        void Title_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.startPoint = e.Location;
+            this.drag = true;
+        }
+
+        void Title_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.drag)
+            { // if we should be dragging it, we need to figure out some movement
+                Point p1 = new Point(e.X, e.Y);
+                Point p2 = this.PointToScreen(p1);
+                Point p3 = new Point(p2.X - this.startPoint.X,
+                                     p2.Y - this.startPoint.Y);
+                this.Location = p3;
+            }
+        }
+
+        private void DaysLeft_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.futureDate = new DateTime(dateTimePicker1.Value.Year, dateTimePicker1.Value.Month,
+                                           dateTimePicker1.Value.Day, dateTimePicker2.Value.Hour,
+                                           dateTimePicker2.Value.Minute, dateTimePicker2.Value.Second);
+            this.label1.Hide();
+            this.button1.Hide();
+            this.dateTimePicker1.Hide();
+            this.dateTimePicker2.Hide();
+            this.CalculateRemainingTime();
+            this.label2.Show();
+            this.button3.Show();
+            Timer timer = new Timer();
+            timer.Interval = (1000); // 1 sec
+            timer.Tick += new EventHandler(Refresh);
+            timer.Start();
+
+        }
+
+        private void Refresh(object sender, EventArgs e)
+        {
+             this.CalculateRemainingTime();
+        }
+
+        private void CalculateRemainingTime()
+        {
+            var diff = this.futureDate - DateTime.Now; 
+            StringBuilder stringBuilder = new StringBuilder(); 
+            if (diff.Days > 0 || diff.Hours > 0 || diff.Minutes > 0 || diff.Seconds > 0)
+            {
+                if (diff.Days > 0)
+                    stringBuilder.Append(diff.Days + " Days\r\n");
+                if (diff.Hours > 0 || diff.Days > 0)
+                    stringBuilder.Append (diff.Hours + " Hours\r\n");
+                if (diff.Days > 0 || diff.Hours > 0 || diff.Minutes > 0)
+                    stringBuilder.Append(diff.Minutes + " Minutes\r\n");
+                if (diff.Days > 0 || diff.Hours > 0 || diff.Minutes > 0 || diff.Seconds > 0)
+                    stringBuilder.Append (diff.Seconds + " Seconds\r\nLeft");
+            }
+            else
+                stringBuilder.Append("0 Seconds");
+
+            this.label2.Text = stringBuilder.ToString() ;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Reset();
+        }
+
+
+        private void Reset()
+        {
+            this.label1.Show();
+            this.button1.Show();
+            this.dateTimePicker1.Show();
+            this.dateTimePicker2.Show();
+            this.dateTimePicker1.Value = DateTime.Now;
+            this.dateTimePicker2.Value = DateTime.Now;
+            this.label2.Hide();
+            this.button3.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
